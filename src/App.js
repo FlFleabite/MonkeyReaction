33a241e2043fa0cac87a971bf9b67e1abc03f0cd
FlFleabite <fleabite_8522@outlook.jp>
2023-12-02 20:36:12 +0900

    add youtube viewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let currentUrl = new URL(window.location.href);
+  const videoId = currentUrl.searchParams.get('v')
+  return <div>
+    <Header />
+    <Body youtubeId={videoId} />
+  </div>
+    ;
+}
+
+function Header() {
+  return <header>
+    <h1>Monkey Reaction</h1>
+  </header>;
+}
+
+function Body(props) {
+  return <body><div>
+      <YoutubeViewer youtubeId={props.youtubeId} />
+      <YoutubeURLInput />
+    </div></body>;
+}
+
+function YoutubeViewer(props) {
+  const youtubeId = props.youtubeId
+  const youtubeURL = youtubeId
+                      ? "https://www.youtube.com/embed/" + youtubeId + "?controls=0&autoplay=1&mute=1"
+                      : "https://www.youtube.com/embed/lsxYH2XQQCg?controls=0&autoplay=1&mute=1"
+  return <iframe id="youtube-embed" width="600" height="340" src={youtubeURL} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>;
+}
+
+function YoutubeURLInput() {
+  return <div>
+    <YoutubeURLTextInput />
+    <YoutubeURLButton />
+  </div>;
+}
+
+function YoutubeURLTextInput(props) {
+  return <input value="" id="youtube-url-input" name="youtube-url-input" type="url" maxLength="512"/>;
+}
+
+function YoutubeURLButton() {
+  function onYoutubeGoButtonClicked() {
+    const urlElement = document.getElementById("youtube-url-input")
+    const urlString = urlElement.value
+
+    if (urlString) {
+      redirectWithYoutubeQuery(urlString)
+    }
+  }
+
+  return <input neme="youtube-url-go-button" type="button" value="GO" onClick={onYoutubeGoButtonClicked} />
+}
+
+function redirectWithYoutubeQuery(youtubeUrl) {
+  var url = new URL(youtubeUrl)
+  const videoId = url.searchParams.get("v")
+
+  let currentUrl = new URL(window.location.href);
+
+  currentUrl.searchParams.set("v", videoId)
+
+  console.log("redirect to:", currentUrl.toString())
+  /* rediect */
+  window.location.href = currentUrl.toString()
 }
 
 export default App;
